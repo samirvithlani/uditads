33f25babaaeb4d9fcb6441dae8b7bffbd613cee9
samirvithlani <samir.vithlani83955@gmail.com>
2023-06-02 12:00:41 +0530

    numpy

---
+++ numpydemo/nump1.py
@@ -0,0 +1,11 @@
+import numpy as np
+#np = np.array([1,2,3,4,5])
+np = np.array([[1,2,3],[4,5,6]],dtype=np.float64)
+#np = np.array([[[1,2,3,5],[3,4,4,55],[5,6,4,55],[5,6,4,55]],[[1,2,3,55],[3,4,4,45],[5,6,4,90],[5,6,4,55]]])
+
+print(np)
+print(type(np))
+print(np.ndim)
+print(np.shape)
+print(np.dtype)
+print(np.size)
---
+++ numpydemo/numpy2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#np = np.zeros((3,4,3),dtype=np.int64)
+#print(np)
+
+
+#np = np.random.random((3,2))
+#np = np.ones((3,4,3),dtype=np.int64)
+#np = np.full((3,4), 1,)
+np = np.arange(10,35,2.5)
+#np = np.random.random(size =(3,2))
+#np = np.ones((3,4,3),dtype=np.int64)
+
+print(np)
