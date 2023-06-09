4a92c627a95cc1632c9bf5e762c6e80d12ab17d1
samirvithlani <samir.vithlani83955@gmail.com>
2023-06-09 11:52:24 +0530

    df head tail

--- pandas/pandas3.py
+++ pandas/pandas3.py
@@ -2,5 +2,13 @@ import pandas as pd
 
 data = pd.read_csv('D:\\royal\\udita-ds\\pandas\\data.csv',index_col = 'Name')
 #show particular column
-data = data['Age']
-print(data)
+# data = data['Age']
+# first = data.loc['Ralf']
+# second = data.loc['Nev']
+# print(first," \n\n",second)
+# print(data)
+x = data.head(2)
+y = data.tail(2)
+print(x)
+print(y)
+
